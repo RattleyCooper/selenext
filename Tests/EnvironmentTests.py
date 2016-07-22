c5053aeffe2fca6eff0c0cbe594d7abef86c8a27
Wykleph <parkeryoung228@gmail.com>
2016-07-22 09:34:12 -0600

    test methods renamed to be more descriptive.

--- Tests/EnvironmentTests.py
+++ Tests/EnvironmentTests.py
@@ -4,26 +4,26 @@ from selenium import webdriver
 from peewee import SqliteDatabase
 
 
-class ConfigLoaderTest(unittest.TestCase):
+class ConfigLoaderEnvironmentTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_env_no_key(self):
+    def test_env_with_undefined_key(self):
         self.assertRaises(KeyError, env, 'DUR')
 
-    def test_env(self):
+    def test_env_with_defined_key(self):
         self.assertEqual(env('BROWSER'), 'chrome')
 
-    def test_env_driver(self):
+    def test_env_driver_with_defined_browser(self):
         self.assertEqual(env_driver(env('BROWSER')), webdriver.Chrome)
 
-    def test_env_driver_no_browser(self):
+    def test_env_driver_undefined_browser(self):
         self.assertEqual(env_driver(env('DB')), False)
 
-    def test_get_database(self):
+    def test_get_database_with_defined_database_type(self):
         self.assertIsInstance(get_database(env('DB_TYPE')), SqliteDatabase)
 
-    def test_get_database_wrong_input(self):
+    def test_get_database_with_undefined_database_type(self):
         self.assertEqual(get_database(env('BROWSER')), False)
 
     def tearDown(self):
