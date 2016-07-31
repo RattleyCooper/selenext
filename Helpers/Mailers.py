6396d3161829c1b777c6197a9ad9a37b41239641
Wykleph <parkeryoung228@gmail.com>
2016-07-31 14:26:45 -0600

    Renamed file / Renamed Class / Validation

--- Helpers/Mailers.py
+++ Helpers/Mailers.py
@@ -2,8 +2,17 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-class Mailer:
+class GMailer:
     def __init__(self, username, password, host, port):
+        if type(username) != str and type(username) != unicode:
+            raise TypeError('A string was expected for the username variable.')
+        if type(password) != str and type(password) != unicode:
+            raise TypeError('A string was expected for the password variable.')
+        if type(host) != str and type(host) != unicode:
+            raise TypeError('A string was expected for the host variable.')
+        if type(port) != int:
+            raise TypeError('A integer was expected for the port variable.')
+
         self.username = username
         self.password = password
         self.host = host
@@ -28,6 +37,7 @@ class Mailer:
         print
         print smtp.sendmail(self.username, email_list, msg)
         smtp.close()
+        return self
 
     def send_email_with_attachment(self, emails, filepath, subject):
         email_list = emails.split(',')
@@ -53,3 +63,4 @@ class Mailer:
         print
         print smtp.sendmail(self.username, email_list, msg)
         smtp.close()
+        return self
