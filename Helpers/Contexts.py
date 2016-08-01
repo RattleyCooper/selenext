5e8c68862bc8d3dd8f4b16d97770547b687e358a
Wykleph <parkeryoung228@gmail.com>
2016-07-31 19:04:48 -0600

    Added / Updated comments.

--- Helpers/Contexts.py
+++ Helpers/Contexts.py
@@ -3,6 +3,13 @@ from contextlib import contextmanager
 
 @contextmanager
 def quitting(thing):
+    """
+    Calls close() and quit() on thing.
+
+    :param thing:
+    :return:
+    """
+
     yield thing
     thing.close()
     try:
