9a4323ea961b0c3fe908ff840190f290837711b9
Wykleph <parkeryoung228@gmail.com>
2016-08-01 10:29:33 -0600

    Updated wording.

--- ThreadedExample.py
+++ ThreadedExample.py
@@ -13,14 +13,16 @@ from time import sleep
 from Project import Models
 # Controllers are kept in the SiteAutomations folder.
 from SiteAutomations.Examples import GoogleExample, BingExample
+# Pull in the command factory, which is the backbone to threaded automations.
 from Helpers.Commands import ThreadedCommandFactory
 
-# Define some controllers to pass to ThreadedCommandManager.
+# Define some controllers to pass to ThreadedCommandFactory.  Note that the
+# controllers being used are subclasses of ThreadedController.
 controllers = {
     'google': GoogleExample.ThreadedGoogleSearch(Models),
     'bing': BingExample.ThreadedBingSearch(Models)  # Check out the example files for more info on threading.
 }
-cmd_mgr = ThreadedCommandFactory(controllers, False)
+cmd_factory = ThreadedCommandFactory(controllers, False)
 
 # Register arguments to pass to each controller.  They are
 # matched by the key in the controllers dictionary.
@@ -38,8 +40,8 @@ search_command_2 = {
 # the command pack as the second parameter.  A Command instance
 # is returned when the threads are created.  These Command
 # objects are used to start the threads.
-cmd1 = cmd_mgr.create_threads(lambda controller, search_term: controller.do_search(search_term), search_command_1)
-cmd2 = cmd_mgr.create_threads(lambda controller, search_term: controller.do_search(search_term), search_command_2)
+cmd1 = cmd_factory.create_threads(lambda controller, search_term: controller.do_search(search_term), search_command_1)
+cmd2 = cmd_factory.create_threads(lambda controller, search_term: controller.do_search(search_term), search_command_2)
 
 # Start the threads.  Each search will be executed, and it will
 # only take as long as the longest loading time.
@@ -51,4 +53,4 @@ print 'finished second search'
 sleep(5)
 
 # Close everything down.
-cmd_mgr.shutdown()
+cmd_factory.shutdown()
