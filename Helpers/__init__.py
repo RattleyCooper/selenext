0c1c0bf658b3f3a6bd8ea859d110042800023632
Wykleph <parkeryoung228@gmail.com>
2016-07-31 19:13:26 -0600

    Added / Updated comments.

--- Helpers/__init__.py
+++ Helpers/__init__.py
@@ -1,6 +1,10 @@
 
 
 class DummyLogger(object):
+    """
+    A logger that does absolutely nothing.  Meant as a drop in replacement for a
+    logger that you would normally get from the logging module.
+    """
     def info(self, *args):
         return self
 
