21a3b171164bc81aa39ce323b851d658e8994f22
Wykleph <parkeryoung228@gmail.com>
2016-07-31 19:20:57 -0600

    Added switchable printing capabilities to DummyLogger.

--- Helpers/__init__.py
+++ Helpers/__init__.py
@@ -5,14 +5,25 @@ class DummyLogger(object):
     A logger that does absolutely nothing.  Meant as a drop in replacement for a
     logger that you would normally get from the logging module.
     """
+    def __init__(self, prints=True):
+        self.prints = prints
+
     def info(self, *args):
+        if self.prints:
+            print args
         return self
 
     def warn(self, *args):
+        if self.prints:
+            print args
         return self
 
     def error(self, *args):
+        if self.prints:
+            print args
         return self
 
     def debug(self, *args):
+        if self.prints:
+            print args
         return self
